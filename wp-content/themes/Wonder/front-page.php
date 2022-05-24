eb56adea746a0a8530738095d9f6de80cf210ec3
Patriekk <94360926+Patriekk@users.noreply.github.com>
2022-05-24 15:00:42 +0200

    initial commit

---
+++ wp-content/themes/Wonder/front-page.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="/wp-content/themes/Wonder/blog-site-templates/css/style.css">
+    <title>Wonder Reviews</title>
+</head>
+
+<body>
+    <nav>
+        <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+        </ul>
+    </nav>
+    <div>
+        
+    </div>
+    <div>
+        <a href="">About Us</a>
+        <a href="">Overview</a>
+    </div>
+</body>
+
+</html>
