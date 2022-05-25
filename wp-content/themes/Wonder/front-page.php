66eeed47f372ed5b00ceaea3c367b88190465c7b
Patriekk <94360926+Patriekk@users.noreply.github.com>
2022-05-25 14:22:08 +0200

    Second try commit

--- wp-content/themes/Wonder/front-page.php
+++ wp-content/themes/Wonder/front-page.php
@@ -8,23 +8,34 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="/wp-content/themes/Wonder/blog-site-templates/css/style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/wp-content//themes/Wonder/blog-site-templates/css/style.css">
     <title>Wonder Reviews</title>
 </head>
 
 <body>
-    <nav>
-        <ul>
-            <li><a href="">Home</a></li>
-            <li><a href="">About Us</a></li>
-        </ul>
+    <nav class="topnav">
+        <a href="#home">Home</a>
+        <a href="#about">About Us</a>
+        <a href="#overview">Overview</a>
+
+        <div class="search-container">
+            <form action="/action_page.php">
+                <input type="text" placeholder="search.." name="search">
+                <button type="submit"><i class="fa fa-search"></i></button>
+            </form>
+        </div>
     </nav>
-    <div>
-        
+    <div class="image-banner">
+        <img class="image-banner" src="/wp-content/themes/Wonder/assets/images/Wonder reviews.png" alt="">
     </div>
+    <div class="style-link">
+        <a class="style-link-buttons" href="">About Us</a>
+        <a class="style-link-buttons" href="">Overview</a>
+    </div>
+    <hr>
     <div>
-        <a href="">About Us</a>
-        <a href="">Overview</a>
+        <h2 class="last-reviews">Last reviews:</h2>
     </div>
 </body>
 
